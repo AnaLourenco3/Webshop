c13a689e402497c1bb8a8794502cb253e2a60c0b
AnaLourenco3 <108146714+AnaLourenco3@users.noreply.github.com>
2022-11-15 15:53:02 +0100

    'imported cors on server.js'

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const { sequelize } = require("./models");
 const express = require("express");
 const productRouter = require("./productRouter");
+const cors = require("cors");
 const app = express();
 const PORT = 4000;
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.use("/products", productRouter);
 
